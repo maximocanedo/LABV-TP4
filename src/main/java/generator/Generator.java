93a9ff80175729fb3281121623e5c68156cb339d
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-06-09 21:29:38 -0300

    Mejoras

--- src/main/java/generator/Generator.java
+++ src/main/java/generator/Generator.java
@@ -3,86 +3,79 @@ package generator;
 import entity.Appointment;
 import entity.Doctor;
 import entity.Patient;
-import entity.Specialty;
 import entity.User;
 
 public class Generator {
+	
+	private static String DELIMITER = "\n**************************\n";
+	
+	private SpecialtyGenerator specialties;
+	private AppointmentGenerator appointments;
+	private DoctorGenerator doctors;
+	private PatientGenerator patients;
+	private UserGenerator users;
 
-    public Generator() {}
-    
-    @Deprecated
-    public Patient generateRandomPaciente() {
-        PatientGenerator pg = new PatientGenerator();
-        return pg.generate();
+	// TODO Reemplazar por beanss.
+    public Generator() {
+    	specialties = new SpecialtyGenerator();
+    	appointments = new AppointmentGenerator();
+    	doctors = new DoctorGenerator();
+    	patients = new PatientGenerator();
+    	users = new UserGenerator();
     }
     
-    @Deprecated
-    public Appointment generateTurnoForDoctor1234(Patient paciente) {
-    	AppointmentGenerator ag = new AppointmentGenerator();
-    	return ag.generateForDoctor1234(paciente);
+    public void generate(int total) {
+    	generate(total, false);
     }
     
-    @Deprecated
-    public User generateRandomUser() {
-        UserGenerator ug = new UserGenerator();
-        return ug.generate();
-    }
-    
-    @Deprecated
-    public Doctor generateRecord(User user) {
-        DoctorGenerator dg = new DoctorGenerator();
-        return dg.generate(user);
-    }
-    
-    @Deprecated
-    public User generateAndSaveRecord() {
-        UserGenerator ug = new UserGenerator();
-        return ug.save();
-    }
-    
-    @Deprecated
-    public Patient generateAndSaveRandomPaciente() {
-        PatientGenerator pg = new PatientGenerator();
-        return pg.save();
-    }
-    
-    @Deprecated
-    public static boolean EXISTE_LEGAJO_1234 = false;
-    
-    @Deprecated
-    public Doctor generateAndSaveRandomDoctor(User user) {
-    	DoctorGenerator dg = new DoctorGenerator();
-    	return dg.save(user);
-    }
-    
-    @Deprecated
-    public User generateAndSaveRandomUser() {
-        UserGenerator ug = new UserGenerator();
-        return ug.save();
-    }
-    
-    @Deprecated
-    public Appointment generateTurno(Patient p, Doctor m) {
-    	AppointmentGenerator ag = new AppointmentGenerator();
-    	return ag.save(p, m);
-    }
-    
-    @Deprecated
-	public Appointment generateTurnoPunto6(Patient p, Doctor m) {
-    	AppointmentGenerator ag = new AppointmentGenerator();
-    	return ag.saveforP6(p, m);
-    }
-    
-    @Deprecated
-    public Specialty[] generateAndSaveRecords() {
-    	SpecialtyGenerator sg = new SpecialtyGenerator();
-    	return sg.save();
-    }
-    
-    @Deprecated
-    public Specialty[] generateRecords() {
-    	SpecialtyGenerator sg = new SpecialtyGenerator();
-    	return sg.generate();
+    public void generate(int total, boolean logEveryRecord) {
+		System.out.println("Se guardarán los objetos Especialidad. ");
+		specialties.save();
+		System.out.println("Se generarán y guardarán " + total + " registros. ");
+
+		StringBuilder usersLog = new StringBuilder();
+		StringBuilder patientsLog = new StringBuilder();
+		StringBuilder doctorsLog = new StringBuilder();
+		StringBuilder appointmentsLog = new StringBuilder();
+		
+		
+		for(int i = 0; i < total; i++) {
+			// Generar
+			User user = users.save();
+			Patient paciente = patients.save();
+			Doctor medico = doctors.save(user);
+			Appointment turno = appointments.saveforP6(paciente, medico);
+			Appointment turnoParaPunto3 = appointments.generateForDoctor1234(paciente);
+			
+			if(logEveryRecord) {
+				usersLog.append(user);
+				patientsLog.append(paciente);
+				doctorsLog.append(medico);
+				appointmentsLog.append(turno);
+				appointmentsLog.append(turnoParaPunto3);
+			}
+		}
+		
+		System.out.println("Se generaron " + total + " registros. ");
+		
+		if(logEveryRecord) {
+			System.out.println(DELIMITER);
+			System.out.println("Usuarios generados: ");
+			System.out.println(usersLog);
+
+			System.out.println(DELIMITER);
+			System.out.println("Médicos generados: ");
+			System.out.println(doctorsLog);
+
+			System.out.println(DELIMITER);
+			System.out.println("Pacientes generados: ");
+			System.out.println(patientsLog);
+
+			System.out.println(DELIMITER);
+			System.out.println("Turnos generados: ");
+			System.out.println(appointmentsLog);
+		}
+		
     }
 
 }
--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,59 +1,33 @@
 package main;
 
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import entity.Appointment;
-import entity.Doctor;
 import entity.Patient;
-import entity.User;
 import generator.Generator;
-import logic.IAppointmentLogic;
-import logic.IDoctorLogic;
-import logicImpl.AppointmentLogicImpl;
-import logicImpl.DoctorLogicImpl;
-import logicImpl.UserLogicImpl;
 import resources.Config;
 
-
-/**
- * Hello world!
- *
- */
 public class App {
 	
-    private UserLogicImpl users;
 	private Generator generator;
 	
 	public App() {
-		users = new UserLogicImpl();
 		generator = new Generator();
 	}
-	
+
+    @SuppressWarnings("resource")
 	public void main() {
-    	// Carga datos
-        @SuppressWarnings("resource")
+    	// Cargar beans
 		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-    	generateFakeRecords(10);
-    	//App.punto1();
-    	//App.punto2();
-    	//App.punto3();
-    	//App.punto4();
-    	//App.punto5();
-    	//App.punto6();
-        
-       // Doctor doctor = (Doctor) context.getBean("doctor");
-        
-        //System.out.println(doctor.toString());
-        
-        Patient paciente = (Patient) context.getBean("paciente");
-        
-        System.out.println(paciente.toString());
 		
+    	// Carga datos
+    	generateFakeRecords(10);
+    	
+    	// Beans
+    	//Doctor doctor = (Doctor) context.getBean("doctor");
+        //System.out.println(doctor.toString());
+        Patient paciente = (Patient) context.getBean("paciente");
+        System.out.println(paciente);
 	}
 	
     public static void main( String[] args ) {
@@ -63,23 +37,7 @@ public class App {
     
    
 	public void generateFakeRecords(int total) {
-		System.out.println("Se guardarán los objetos Especialidad. ");
-		generator.generateAndSaveRecords();
-		System.out.println("Se generarán y guardarán " + total + " registros. ");
-		for(int i = 0; i < total; i++) {
-			User user = generator.generateAndSaveRandomUser();
-			System.out.println(" - " + i + " usuario/s de " + total + ": " + user.getName() + "(@" + user.getUsername() + ")\n");
-			Patient paciente = generator.generateAndSaveRandomPaciente();
-			System.out.println(" - " + i + " paciente/s de " + total + ": " + paciente.getName() + "; DNI N.º: " + paciente.getDni());
-			Doctor medico = generator.generateAndSaveRandomDoctor(user);
-			System.out.println(medico);
-			System.out.println(" - " + i + " médico/s de " + total + ": " + medico.getName() + "; Legajo N.º: " + medico.getFile());
-			Appointment turno = generator.generateTurnoPunto6(paciente, medico);
-			System.out.println(turno);
-			Appointment turnoParaPunto3 = generator.generateTurnoForDoctor1234(paciente);
-			System.out.println(turnoParaPunto3);
-		}
-		System.out.println("Se generaron " + total + " registros. ");
+		generator.generate(total, true);
 	}
 	
 	
