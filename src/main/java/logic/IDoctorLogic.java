0180e19e9e0740415d5dbcff308eed3685b14c9c
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-06-21 12:55:21 -0300

    Implementar permisos #17

--- src/main/java/logic/IDoctorLogic.java
+++ src/main/java/logic/IDoctorLogic.java
@@ -3,71 +3,72 @@ package logic;
 import java.time.LocalDate;
 import java.util.List;
 import entity.Optional;
+import entity.User;
 import exceptions.NotFoundException;
 import entity.Doctor;
 
 public interface IDoctorLogic {
-
+	
 	/**
      * Agrega un medico a la base de datos.
      * @param medico datos del medico a agregar.
      */
-    void add(Doctor medico);
+    void add(Doctor medico, User requiring);
 
     /**
      * Busca un médico por su ID.
      * @param id ID del medico.
      */
-    Optional<Doctor> findById(int id);
+    Optional<Doctor> findById(int id, User requiring);
     
     /**
      * Busca un médico por su ID.
      * @param id ID del medico.
      */
-    Optional<Doctor> findById(int id, boolean includeInactive);
+    Optional<Doctor> findById(int id, boolean includeInactive, User requiring);
     
     /**
      * Busca un médico por su legajo.
      * @param file Legajo
      */
-    Optional<Doctor> findByFile(int file);
+    Optional<Doctor> findByFile(int file, User requiring);
     
     /**
      * Lista todos los medicos de la base de datos, paginable.
      * @param page Numero de pagina (De 1 en adelante)
      * @param size Cantidad de elementos por pagina.
      */
-    List<Doctor> list(int page, int size, boolean includeInactive);
+    List<Doctor> list(int page, int size, boolean includeInactive, User requiring);
     
     /**
      * Lista todos los medicos de la base de datos, paginable.
      * @param page Numero de pagina (De 1 en adelante)
      * @param size Cantidad de elementos por pagina.
      */
-    List<Doctor> list(int page, int size);
+    List<Doctor> list(int page, int size, User requiring);
 
     /**
      * Lista todos los medicos de la base de datos.
      */
-    List<Doctor> list();
+    List<Doctor> list(User requiring);
     
     /**
      * Lista los legajos de los medicos de la base de datos.
      */
-    List<Object[]> listOnlyFileNumbersAndNames();
+    List<Object[]> listOnlyFileNumbersAndNames(User requiring);
     
     /**
      * Obtiene todos los legajos de los medicos 
      * @return Lista de legajos.
      */
-    List<Integer> listOnlyFileNumbers();
+    List<Integer> listOnlyFileNumbers(User requiring);
 
     /**
      * Devuelve una lista con el legajo, la fecha y el estado de cada turno asignado a un médico en específico durante la fecha indicada.
      * @param Legajo del médico.
      * @param Fecha deseada.
      */
-    List<Object[]> getAppointmentsByDoctorAndDate(int fileNumber, LocalDate fecha);
+    List<Object[]> getAppointmentsByDoctorAndDate(int fileNumber, LocalDate fecha, User requiring);
     
     /**
      * Devuelve una lista con el legajo, la fecha y el estado de cada turno asignado a un médico en específico dentro del rango de fechas indicado.
@@ -75,29 +76,29 @@ public interface IDoctorLogic {
      * @param startDate Fecha límite.
      * @param endDate Segunda fecha límite.
      */
-    List<Object[]> getAppointmentsByDoctorAndDateRange(int fileNumber, LocalDate startDate, LocalDate endDate);
+    List<Object[]> getAppointmentsByDoctorAndDateRange(int fileNumber, LocalDate startDate, LocalDate endDate, User requiring);
     
     /**
      * Obtiene el medico con el legajo de mayor valor de la base de datos.
      */
-	Doctor findDoctorWithHighestFileNumber();
+	Doctor findDoctorWithHighestFileNumber(User requiring);
 
 	/**
      * Lista todos los médicos de la base de datos, paginable.
      * @param page Número de página (De 1 en adelante)
      * @param size Cantidad de elementos por página.
      */
-	List<Doctor> listOrderByFileDescending(int page, int size);
+	List<Doctor> listOrderByFileDescending(int page, int size, User requiring);
 
 	/**
      * Lista todos los médicos de la base de datos.
      */
-	List<Doctor> listOrderByFileDescending();
+	List<Doctor> listOrderByFileDescending(User requiring);
 	
 	/**
 	 * Actualiza un médico de la base de datos.
 	 */
-	void update(Doctor medico) throws NotFoundException;
+	void update(Doctor medico, User requiring) throws NotFoundException;
 	
 	/**
 	 * Elimina permanentemente un médico de la base de datos.
@@ -108,10 +109,131 @@ public interface IDoctorLogic {
 	/**
 	 * Deshabilita un médico de la base de datos.
 	 */
+	void disable(int id, User requiring) throws NotFoundException;
+	
+	/**
+	 * Rehabilita un médico de la base de datos.
+	 */
+	void enable(int id, User requiring) throws NotFoundException;
+	
+	/** # Deprecated methods **/
+
+	/**
+     * Agrega un medico a la base de datos.
+     * @param medico datos del medico a agregar.
+     */
+	@Deprecated
+    void add(Doctor medico);
+
+    /**
+     * Busca un médico por su ID.
+     * @param id ID del medico.
+     */
+	@Deprecated
+    Optional<Doctor> findById(int id);
+    
+    /**
+     * Busca un médico por su ID.
+     * @param id ID del medico.
+     */
+	@Deprecated
+    Optional<Doctor> findById(int id, boolean includeInactive);
+    
+    /**
+     * Busca un médico por su legajo.
+     * @param file Legajo
+     */
+	@Deprecated
+    Optional<Doctor> findByFile(int file);
+    
+    /**
+     * Lista todos los medicos de la base de datos, paginable.
+     * @param page Numero de pagina (De 1 en adelante)
+     * @param size Cantidad de elementos por pagina.
+     */
+	@Deprecated
+    List<Doctor> list(int page, int size, boolean includeInactive);
+    
+    /**
+     * Lista todos los medicos de la base de datos, paginable.
+     * @param page Numero de pagina (De 1 en adelante)
+     * @param size Cantidad de elementos por pagina.
+     */
+	@Deprecated
+    List<Doctor> list(int page, int size);
+
+    /**
+     * Lista todos los medicos de la base de datos.
+     */
+	@Deprecated
+    List<Doctor> list();
+    
+    /**
+     * Lista los legajos de los medicos de la base de datos.
+     */
+	@Deprecated
+    List<Object[]> listOnlyFileNumbersAndNames();
+    
+    /**
+     * Obtiene todos los legajos de los medicos 
+     * @return Lista de legajos.
+     */
+	@Deprecated
+    List<Integer> listOnlyFileNumbers();
+
+    /**
+     * Devuelve una lista con el legajo, la fecha y el estado de cada turno asignado a un médico en específico durante la fecha indicada.
+     * @param Legajo del médico.
+     * @param Fecha deseada.
+     */
+	@Deprecated
+    List<Object[]> getAppointmentsByDoctorAndDate(int fileNumber, LocalDate fecha);
+    
+    /**
+     * Devuelve una lista con el legajo, la fecha y el estado de cada turno asignado a un médico en específico dentro del rango de fechas indicado.
+     * @param Legajo del médico
+     * @param startDate Fecha límite.
+     * @param endDate Segunda fecha límite.
+     */
+	@Deprecated
+    List<Object[]> getAppointmentsByDoctorAndDateRange(int fileNumber, LocalDate startDate, LocalDate endDate);
+    
+    /**
+     * Obtiene el medico con el legajo de mayor valor de la base de datos.
+     */
+	@Deprecated
+	Doctor findDoctorWithHighestFileNumber();
+
+	/**
+     * Lista todos los médicos de la base de datos, paginable.
+     * @param page Número de página (De 1 en adelante)
+     * @param size Cantidad de elementos por página.
+     */
+	@Deprecated
+	List<Doctor> listOrderByFileDescending(int page, int size);
+
+	/**
+     * Lista todos los médicos de la base de datos.
+     */
+	@Deprecated
+	List<Doctor> listOrderByFileDescending();
+	
+	/**
+	 * Actualiza un médico de la base de datos.
+	 */
+	@Deprecated
+	void update(Doctor medico) throws NotFoundException;
+	
+	
+	/**
+	 * Deshabilita un médico de la base de datos.
+	 */
+	@Deprecated
 	void disable(int id) throws NotFoundException;
 	
 	/**
 	 * Rehabilita un médico de la base de datos.
 	 */
+	@Deprecated
 	void enable(int id) throws NotFoundException;
 }
--- src/main/java/logicImpl/DoctorLogicImpl.java
+++ src/main/java/logicImpl/DoctorLogicImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 import dao.IDoctorDAO;
 import entity.Doctor;
 import entity.Optional;
+import entity.Permit;
+import entity.User;
 import exceptions.NotFoundException;
 import logic.IDoctorLogic;
 
@@ -17,81 +19,99 @@ public class DoctorLogicImpl implements IDoctorLogic {
 
 	@Autowired
     private IDoctorDAO doctorsrepository;
+	
+	@Autowired
+	private UserPermitLogicImpl permits;
 
     public DoctorLogicImpl() {}
 
     @Override
-    public void add(Doctor medico) {
+    public void add(Doctor medico, User requiring) {
+    	permits.require(requiring, Permit.CREATE_DOCTOR);
         doctorsrepository.add(medico);
     }
 
     @Override
-    public Optional<Doctor> findById(int id) {
+    public Optional<Doctor> findById(int id, User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR);
         return doctorsrepository.findById(id);
     }
 
     @Override
-    public List<Doctor> list(int page, int size) {
+    public List<Doctor> list(int page, int size, User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR);
         return doctorsrepository.list(page, size);
     }
 
     @Override
-    public List<Doctor> list() {
+    public List<Doctor> list(User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR);
         return list(1, 15);
     }
 
 	@Override
-	public List<Object[]> listOnlyFileNumbersAndNames() {
+	public List<Object[]> listOnlyFileNumbersAndNames(User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR);
 		return doctorsrepository.listOnlyFileNumbersAndNames();
 	}
 	
 	@Override
-    public List<Integer> listOnlyFileNumbers(){
+    public List<Integer> listOnlyFileNumbers(User requiring){
+    	permits.require(requiring, Permit.READ_DOCTOR);
 		return doctorsrepository.listOnlyFileNumbers();
 	}
 	
 	@Override
-    public Doctor findDoctorWithHighestFileNumber() {
+    public Doctor findDoctorWithHighestFileNumber(User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR);
 		return doctorsrepository.findDoctorWithHighestFileNumber();
 	}
 	
 	@Override
-    public List<Doctor> listOrderByFileDescending(int page, int size) {
+    public List<Doctor> listOrderByFileDescending(int page, int size, User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR);
 		return doctorsrepository.listOrderByFileDescending(page, size);
 	}
 	
 	@Override
-    public List<Doctor> listOrderByFileDescending() {
-		return this.listOrderByFileDescending(1, 10);
+    public List<Doctor> listOrderByFileDescending(User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR);
+		return this.listOrderByFileDescending(1, 10, requiring);
 	}
 	
 	@Override
-    public List<Object[]> getAppointmentsByDoctorAndDate(int legajo, LocalDate fecha) {
+    public List<Object[]> getAppointmentsByDoctorAndDate(int legajo, LocalDate fecha, User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR_APPOINTMENTS);
         return doctorsrepository.getAppointmentsByDoctorAndDate(legajo, fecha);
     }
 	
 	@Override
-    public List<Object[]> getAppointmentsByDoctorAndDateRange(int legajo, LocalDate fechaInicio, LocalDate fechaFin) {
+    public List<Object[]> getAppointmentsByDoctorAndDateRange(int legajo, LocalDate fechaInicio, LocalDate fechaFin, User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR_APPOINTMENTS);
         return doctorsrepository.getAppointmentsByDoctorAndDateRange(legajo, fechaInicio, fechaFin);
     }
 
 	@Override
-	public Optional<Doctor> findByFile(int file) {
+	public Optional<Doctor> findByFile(int file, User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR);
 		return doctorsrepository.findByFile(file);
 	}
 
 	@Override
-	public Optional<Doctor> findById(int id, boolean includeInactive) {
+	public Optional<Doctor> findById(int id, boolean includeInactive, User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR);
 		return doctorsrepository.findById(id, includeInactive);
 	}
 
 	@Override
-	public List<Doctor> list(int page, int size, boolean includeInactive) {
+	public List<Doctor> list(int page, int size, boolean includeInactive, User requiring) {
+    	permits.require(requiring, Permit.READ_DOCTOR);
 		return doctorsrepository.list(page, size, includeInactive);
 	}
 
 	@Override
-	public void update(Doctor medico) throws NotFoundException {
+	public void update(Doctor medico, User requiring) throws NotFoundException {
+    	permits.require(requiring, Permit.UPDATE_DOCTOR_PERSONAL_DATA);
     	Optional<Doctor> search = findById(medico.getId());
     	if(search.isEmpty()) throw new NotFoundException();
     	Doctor original = search.get();
@@ -115,12 +135,132 @@ public class DoctorLogicImpl implements IDoctorLogic {
 	}
 
 	@Override
+	public void disable(int id, User requiring) throws NotFoundException {
+    	permits.require(requiring, Permit.DISABLE_DOCTOR);
+		doctorsrepository.disable(id);
+		
+	}
+
+	@Override
+	public void enable(int id, User requiring) throws NotFoundException {
+    	permits.require(requiring, Permit.ENABLE_DOCTOR);
+		doctorsrepository.disable(id);
+	}
+	
+	/** # Deprecated methods **/
+    
+    @Override
+    @Deprecated
+    public void add(Doctor medico) {
+        doctorsrepository.add(medico);
+    }
+
+    @Override
+    @Deprecated
+    public Optional<Doctor> findById(int id) {
+        return doctorsrepository.findById(id);
+    }
+
+    @Override
+    @Deprecated
+    public List<Doctor> list(int page, int size) {
+        return doctorsrepository.list(page, size);
+    }
+
+    @Override
+    @Deprecated
+    public List<Doctor> list() {
+        return list(1, 15);
+    }
+
+	@Override
+    @Deprecated
+	public List<Object[]> listOnlyFileNumbersAndNames() {
+		return doctorsrepository.listOnlyFileNumbersAndNames();
+	}
+	
+	@Override
+    @Deprecated
+    public List<Integer> listOnlyFileNumbers(){
+		return doctorsrepository.listOnlyFileNumbers();
+	}
+	
+	@Override
+    @Deprecated
+    public Doctor findDoctorWithHighestFileNumber() {
+		return doctorsrepository.findDoctorWithHighestFileNumber();
+	}
+	
+	@Override
+    @Deprecated
+    public List<Doctor> listOrderByFileDescending(int page, int size) {
+		return doctorsrepository.listOrderByFileDescending(page, size);
+	}
+	
+	@Override
+    @Deprecated
+    public List<Doctor> listOrderByFileDescending() {
+		return this.listOrderByFileDescending(1, 10);
+	}
+	
+	@Override
+    @Deprecated
+    public List<Object[]> getAppointmentsByDoctorAndDate(int legajo, LocalDate fecha) {
+        return doctorsrepository.getAppointmentsByDoctorAndDate(legajo, fecha);
+    }
+	
+	@Override
+    @Deprecated
+    public List<Object[]> getAppointmentsByDoctorAndDateRange(int legajo, LocalDate fechaInicio, LocalDate fechaFin) {
+        return doctorsrepository.getAppointmentsByDoctorAndDateRange(legajo, fechaInicio, fechaFin);
+    }
+
+	@Override
+    @Deprecated
+	public Optional<Doctor> findByFile(int file) {
+		return doctorsrepository.findByFile(file);
+	}
+
+	@Override
+    @Deprecated
+	public Optional<Doctor> findById(int id, boolean includeInactive) {
+		return doctorsrepository.findById(id, includeInactive);
+	}
+
+	@Override
+    @Deprecated
+	public List<Doctor> list(int page, int size, boolean includeInactive) {
+		return doctorsrepository.list(page, size, includeInactive);
+	}
+
+	@Override
+    @Deprecated
+	public void update(Doctor medico) throws NotFoundException {
+    	Optional<Doctor> search = findById(medico.getId());
+    	if(search.isEmpty()) throw new NotFoundException();
+    	Doctor original = search.get();
+    	if (medico.getName() != null) original.setName(medico.getName());
+        if (medico.getSurname() != null) original.setSurname(medico.getSurname());
+        if (medico.getSex() != null) original.setSex(medico.getSex());
+        if (medico.getBirth() != null) original.setBirth(medico.getBirth());
+        if (medico.getAddress() != null) original.setAddress(medico.getAddress());
+        if (medico.getLocalty() != null) original.setLocalty(medico.getLocalty());
+        if (medico.getEmail() != null) original.setEmail(medico.getEmail());
+        if (medico.getPhone() != null) original.setPhone(medico.getPhone());
+        if (medico.getSpecialty() != null) original.setSpecialty(medico.getSpecialty());
+        if (medico.getUser() != null) original.setUser(medico.getUser());
+		doctorsrepository.update(original);
+	}
+
+	@Override
+    @Deprecated
 	public void disable(int id) throws NotFoundException {
 		doctorsrepository.disable(id);
 		
 	}
 
 	@Override
+    @Deprecated
 	public void enable(int id) throws NotFoundException {
 		doctorsrepository.disable(id);
 	}
