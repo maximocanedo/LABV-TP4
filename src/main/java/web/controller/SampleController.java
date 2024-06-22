713ea320afa4b86fcb7db9c9c6f7817497fa0a40
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-06-22 20:15:37 -0300

    Solucionar problema con controladores

--- src/main/java/web/controller/SampleController.java
+++ src/main/java/web/controller/SampleController.java
@@ -1,6 +1,14 @@
 package web.controller;
 
-@Controller
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/TP4_GRUPO3/api")
 public class SampleController {
 
+	@GetMapping
+	public String getSomething() {
+		return "ALALALA";
+	}
+	
 }
