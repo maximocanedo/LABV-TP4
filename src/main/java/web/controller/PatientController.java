16068f3f9c2ff87f38797df8a872176bc62ce6e8
Lucas-G-Lunix <lucasgruden0@protonmail.com>
2024-06-25 14:44:02 -0300

    Se creo el metodo par elimiminar logicamente un paciente

--- src/main/java/web/controller/PatientController.java
+++ src/main/java/web/controller/PatientController.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -62,8 +64,11 @@ public class PatientController {
 			return patients.update(patient, requiring);
 		}
 		
-		
-		// Acciones Paciente Actual
-		
+		@DeleteMapping("/p/{id}")
+		public ResponseEntity<?> disable(@PathVariable int id, HttpServletRequest req, HttpServletResponse res) {
+			User requiring = auth.require(req, res);
+			patients.disable(id, requiring);;
+			return ResponseEntity.status(200).build();
+		}
 
 }
