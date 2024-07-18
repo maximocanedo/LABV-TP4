2cf2f1482a6bfbdd35f853cdaad0f841526d4453
Lucas-G-Lunix <lucasgruden0@protonmail.com>
2024-07-18 17:37:27 -0300

    Listado y modificacion medicos

--- WebContent/api/controller/medicos/modificarMedico.js
+++ WebContent/api/controller/medicos/modificarMedico.js
@@ -1,29 +1,29 @@
 import * as headerAdminService from "../services/headerAdminService.js";
 import { createStore } from "../../lib/redux.js";
 import { ElementBuilder } from "../dom.js";
-import { findById } from "../../actions/patients.js";
+import { findById } from "../../actions/doctors.js";
 import { login } from "../../actions/users.js";
 
 const event = {
     UPDATE_NAME: "UPDATE_NAME",
     UPDATE_SURNAME: "UPDATE_SURNAME",
-    UPDATE_DNI: "UPDATE_DNI",
     UPDATE_EMAIL: "UPDATE_EMAIL",
-    UPDATE_PHONE: "UPDATE_PHONE",
+    UPDATE_ADDRESS: "UPDATE_ADDRESS",
+    UPDATE_SPECIALTY: "UPDATE_SPECIALTY"
 };
 
-const reducer = (state = { name: "", surname: "", dni: "", email: "", phone: "" }, action) => {
+const reducer = (state = { name: "", surname: "", email: "", address: "", specialty: "" }, action) => {
     switch(action.type) {
         case event.UPDATE_NAME:
             return { ...state, name: action.payload };
         case event.UPDATE_SURNAME:
             return { ...state, surname: action.payload };
-        case event.UPDATE_DNI:
-            return { ...state, dni: action.payload };
         case event.UPDATE_EMAIL:
             return { ...state, email: action.payload };
-        case event.UPDATE_PHONE:
-            return { ...state, phone: action.payload };
+        case event.UPDATE_ADDRESS:
+            return { ...state, address: action.payload };
+        case event.UPDATE_SPECIALTY:
+            return { ...state, specialty: action.payload };
         default:
             return { ...state };
     }
@@ -32,32 +32,29 @@ const reducer = (state = { name: "", surname: "", dni: "", email: "", phone: "" 
 const store = createStore(reducer, {
     name: "", 
     surname: "",
-    dni: "",
-    email: "", 
-    phone: ""
+    email: "",
+    address: "",
+    specialty: ""
 });
 
 const formRegister = document.getElementById("formRegister");
 
 const txtName = ElementBuilder.from(document.getElementById("txtName")).linkValue(store, event.UPDATE_NAME, "name");
 const txtSurname = ElementBuilder.from(document.getElementById("txtSurname")).linkValue(store, event.UPDATE_SURNAME, "surname");
-const txtDNI = ElementBuilder.from(document.getElementById("txtDNI")).linkValue(store, event.UPDATE_DNI, "dni");
-const txtEmail = ElementBuilder.from(document.getElementById("txtEmail")).linkValue(store, event.UPDATE_EMAIL, "email");
-const txtPhone = ElementBuilder.from(document.getElementById("txtPhone")).linkValue(store, event.UPDATE_PHONE, "phone");
+const txtEmail = ElementBuilder.from(document.getElementById("txtEmail")).linkValue(store, event.UPDATE_SURNAME, "email");
+const txtAddress = ElementBuilder.from(document.getElementById("txtAddress")).linkValue(store, event.UPDATE_SURNAME, "address");
+const txtSpecialty = ElementBuilder.from(document.getElementById("txtSpecialty")).linkValue(store, event.UPDATE_SPECIALTY, "specialty");
 
 addEventListener("DOMContentLoaded", async (event) => {
     const user = await login("alicia.schimmel", "12345678");
     const header = headerAdminService.load();
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const patient = await findById(parseInt(urlParams.get("Id")));
-    console.log(patient)
-    txtName.value(patient.name);
-    txtSurname.value(patient.surname);
-    txtDNI.value(patient.dni);
-    // @ts-ignore
-    txtEmail.value(patient.email);
-    // @ts-ignore
-    txtPhone.value(patient.phone);
+    const doctor = await findById(parseInt(urlParams.get("Id")));
+    txtName.value(doctor.name);
+    txtSurname.value(doctor.surname);
+    txtSpecialty.value(doctor.specialty.name);
+    txtEmail.value(doctor.email);
+    txtAddress.value(doctor.address);
 })
 
