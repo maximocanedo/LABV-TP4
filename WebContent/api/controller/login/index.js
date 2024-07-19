5bc03ec362c4b444e275f732db62267279f58596
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-07-19 17:04:23 -0300

    Update index.js

--- WebContent/api/controller/login/index.js
+++ WebContent/api/controller/login/index.js
@@ -9,13 +9,18 @@ const txtPasswordEB = ElementBuilder.from(txtPassword);
 const btnLogin = ElementBuilder.from(document.getElementById("btnLogin"));
 const formLogin = ElementBuilder.from(document.getElementById("formLogin"));
 
+export const resolveLocalUrl = (relativeUrl) => {
+    const PREFIX = "pages";
+    if(window.location.port == '81')
+        return resolveUrl(PREFIX + (!relativeUrl.startsWith("/") ? "/": "") + relativeUrl);
+    return (!relativeUrl.startsWith("/") ? "/": "") + relativeUrl;
+}
+
 const onValid = () => {
     clearVal();
-    const PREFIX = ""; // "pages";
-    const HOME = "/";
     const search = new URLSearchParams(window.location.search);
-    const next = search.get("next")?? HOME;
-    window.location.href = resolveUrl(PREFIX + (!next.startsWith("/") ? "/": "") + next);
+    const next = search.get("next")?? "/";
+    window.location.href = resolveLocalUrl(next);
 };
 
 const clearVal = () => {
