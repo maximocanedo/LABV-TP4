c17e789fc3d7992b63e9bbea309d6231d09fd4b3
Lucas-G-Lunix <lucasgruden0@protonmail.com>
2024-07-19 11:50:04 -0300

    Nueva implementacion paciente

--- WebContent/api/controller/pacientes/modificarPaciente.js
+++ WebContent/api/controller/pacientes/modificarPaciente.js
@@ -4,15 +4,21 @@ import { ElementBuilder } from "../dom.js";
 import { findById } from "../../actions/patients.js";
 import { login } from "../../actions/users.js";
 
+let patient;
+
 const event = {
     UPDATE_NAME: "UPDATE_NAME",
     UPDATE_SURNAME: "UPDATE_SURNAME",
     UPDATE_DNI: "UPDATE_DNI",
-    UPDATE_EMAIL: "UPDATE_EMAIL",
     UPDATE_PHONE: "UPDATE_PHONE",
+    UPDATE_EMAIL: "UPDATE_EMAIL",
+    UPDATE_ADDRESS: "UPDATE_ADDRESS",
+    UPDATE_LOCALTY: "UPDATE_LOCALTY",
+    UPDATE_PROVINCE: "UPDATE_PROVINCE",
+    UPDATE_BIRTH: "UPDATE_BIRTH"
 };
 
-const reducer = (state = { name: "", surname: "", dni: "", email: "", phone: "" }, action) => {
+const reducer = (state = { name: "", surname: "", dni: "", email: "", phone: "", address: "", localty: "", province: "", birth: "" }, action) => {
     switch(action.type) {
         case event.UPDATE_NAME:
             return { ...state, name: action.payload };
@@ -24,39 +30,39 @@ const reducer = (state = { name: "", surname: "", dni: "", email: "", phone: "" 
             return { ...state, email: action.payload };
         case event.UPDATE_PHONE:
             return { ...state, phone: action.payload };
+        case event.UPDATE_ADDRESS:
+            return { ...state, address: action.payload };
+        case event.UPDATE_LOCALTY:
+            return { ...state, localty: action.payload };
+        case event.UPDATE_PROVINCE:
+            return { ...state, province: action.payload };
+        case event.UPDATE_BIRTH:
+            return { ...state, birth: action.payload };
         default:
             return { ...state };
     }
 };
 
-const store = createStore(reducer, {
-    name: "", 
-    surname: "",
-    dni: "",
-    email: "", 
-    phone: ""
-});
-
-const formRegister = document.getElementById("formRegister");
-
-const txtName = ElementBuilder.from(document.getElementById("txtName")).linkValue(store, event.UPDATE_NAME, "name");
-const txtSurname = ElementBuilder.from(document.getElementById("txtSurname")).linkValue(store, event.UPDATE_SURNAME, "surname");
-const txtDNI = ElementBuilder.from(document.getElementById("txtDNI")).linkValue(store, event.UPDATE_DNI, "dni");
-const txtEmail = ElementBuilder.from(document.getElementById("txtEmail")).linkValue(store, event.UPDATE_EMAIL, "email");
-const txtPhone = ElementBuilder.from(document.getElementById("txtPhone")).linkValue(store, event.UPDATE_PHONE, "phone");
-
-(async () => {
+const load = async () => {
     const user = await login("alicia.schimmel", "12345678");
     const header = headerAdminService.load();
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const patient = await findById(parseInt(urlParams.get("Id")));
-    store.dispatch({type: event.UPDATE_NAME, payload: patient.name});
-    store.dispatch({type: event.UPDATE_SURNAME, payload: patient.surname});
-    store.dispatch({type: event.UPDATE_DNI, payload: patient.dni});
-    // @ts-ignore
-    store.dispatch({type: event.UPDATE_EMAIL, payload: patient.email});
-    // @ts-ignore
-    store.dispatch({type: event.UPDATE_PHONE, payload: patient.phone});
-})();
+    patient = await findById(parseInt(urlParams.get("Id")));
+};
+
+load().then(() => {
+    const store = createStore(reducer, patient);
+    const formRegister = document.getElementById("formModificarPaciente");
+
+    const txtName = ElementBuilder.from(document.getElementById("txtName")).linkValue(store, event.UPDATE_NAME, "name");
+    const txtSurname = ElementBuilder.from(document.getElementById("txtSurname")).linkValue(store, event.UPDATE_SURNAME, "surname");
+    const txtDNI = ElementBuilder.from(document.getElementById("txtDNI")).linkValue(store, event.UPDATE_DNI, "dni");
+    const txtEmail = ElementBuilder.from(document.getElementById("txtEmail")).linkValue(store, event.UPDATE_EMAIL, "email");
+    const txtPhone = ElementBuilder.from(document.getElementById("txtPhone")).linkValue(store, event.UPDATE_PHONE, "phone");
+    const txtAddress = ElementBuilder.from(document.getElementById("txtAddress")).linkValue(store, event.UPDATE_ADDRESS, "address");
+    const txtLocalty = ElementBuilder.from(document.getElementById("txtLocalty")).linkValue(store, event.UPDATE_LOCALTY, "localty");
+    const txtProvince = ElementBuilder.from(document.getElementById("txtProvince")).linkValue(store, event.UPDATE_PROVINCE, "province");
+    const txtBirth = ElementBuilder.from(document.getElementById("txtBirth")).linkValue(store, event.UPDATE_BIRTH, "birth");
+})
 
