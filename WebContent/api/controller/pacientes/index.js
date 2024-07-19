606dcfd16a75196060f75955ff42cb05c5def1bc
Lucas-G-Lunix <lucasgruden0@protonmail.com>
2024-07-19 18:59:30 -0300

    cambios boton activar/desactivar

--- WebContent/api/controller/pacientes/index.js
+++ WebContent/api/controller/pacientes/index.js
@@ -10,7 +10,7 @@ let dataTablePacientes;
 
 (async () => {
     const user = await login("alicia.schimmel", "12345678");
-    const pacientes = await new Query().paginate(1, 100).filterByStatus(FilterStatus.BOTH).search();    
+    const pacientes = await new Query().paginate(0, 10).filterByStatus(FilterStatus.BOTH).search();    
     // @ts-ignore
     dataTablePacientes = new DataTable('#tableListadoPacientes', {
         columns: [
@@ -23,14 +23,11 @@ let dataTablePacientes;
             { data: 'active', render: function ( data, type, row ) {
                 return data ? "Activo" : "Inactivo";
             }},
-            { data: 'id', render: function ( data, type, row ) {
-                return `<form action="./modificarPaciente.html?Id=${data}" method="post"><button type="submit" class="btn btn-primary">Modificar Paciente</button></form>`;
+            { data: '', render: function ( data, type, row ) {
+                return `<form action="./modificarPaciente.html?Id=${row.id}" method="post"><button type="submit" class="btn btn-primary">Modificar Paciente</button></form>`;
             }},
-            { data: 'id', render: function ( data, type, row ) {
-                return `<button type="button" class="btn btn-primary" onclick="enablePatient(${data});">Activar</button>`;
-            }},
-            { data: 'id', render: function ( data, type, row ) {
-                return `<button type="button" class="btn btn-primary" onclick="disablePatient(${data});">Desactivar</button>`;
+            { data: '', render: function ( data, type, row ) {
+                return `<button type="button" class="btn btn-primary" onclick="${row.active ? "disable" : "enable"}Patient(${row.id});">${row.active ? "Desactivar" : "Activar"}</button>`;
             }}
         ],
         data: pacientes,
@@ -40,7 +37,7 @@ let dataTablePacientes;
 const dataTableUpdate = async () => {
     const actualPage = dataTablePacientes.page();
     dataTablePacientes.clear();
-    const pacientes = await new Query().paginate(1, 100).filterByStatus(FilterStatus.BOTH).search();    
+    const pacientes = await new Query().paginate(0, 10).filterByStatus(FilterStatus.BOTH).search();    
     dataTablePacientes.rows.add(pacientes);
     dataTablePacientes.draw()
     dataTablePacientes.page(actualPage).draw("page");
@@ -48,13 +45,16 @@ const dataTableUpdate = async () => {
 
 
 const enablePatient = async (id) => {
-    const enableResponse = await enable(id);
-    dataTableUpdate()
+    const enableResponse = await enable(id).then(() => {
+        dataTableUpdate()
+    })
+    
 }
 
 const disablePatient = async (id) => {
-    const disableResponse = await disable(id);
-    dataTableUpdate();
+    const disableResponse = await disable(id).then(() => {
+        dataTableUpdate()
+    })
 }
 
 // @ts-ignore
