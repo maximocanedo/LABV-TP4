c94adae4f47eba210bac00ea16ae69db4e0d6382
Lucas-G-Lunix <lucasgruden0@protonmail.com>
2024-07-23 09:52:02 -0300

    se agrego al listado de doctor la capacidad de dar presentes y ausentes

--- WebContent/api/controller/appointments/index.js
+++ WebContent/api/controller/appointments/index.js
@@ -1,5 +1,5 @@
 import { Toast } from './../../lib/toast.js';
-import { Query, enable, disable } from '../../actions/appointments.js';
+import { Query, update, findById } from '../../actions/appointments.js';
 import * as headerDoctorService from "../services/headerDoctorService.js";
 import { FilterStatus } from './../../actions/users.js';
 
@@ -11,7 +11,6 @@ let isLoading = true;
 const ddlEntriesPerPage = document.getElementById("ddlEntriesPerPage");
 const txtBuscar = document.getElementById("txtBuscar");
 const btnBuscar = document.getElementById("btnBuscar");
-const ddlStatusFilter = document.getElementById("ddlStatusFilter");
 const ddlStatusFilterAppointment = document.getElementById("ddlStatusFilterAppointment");
 // Table
 const loadingSpinner = document.getElementById("loadingSpinner");
@@ -24,51 +23,61 @@ const btnNextPage = document.getElementById("btnNextPage");
 })();
 
 const load = async () => {
-    
+
     const appointments = await new Query()
         .paginate(page, parseInt(ddlEntriesPerPage.value))
         .filterByAppointmentStatus(ddlStatusFilterAppointment.value)
-        .filterByStatus(FilterStatus[ddlStatusFilter.value])
-        .search();    
+        .filterByDateBetween(new Date("2025-01-01").toISOString().split("T")[0], new Date("2025-02-01").toISOString().split("T")[0])
+        //.filterByDoctor()
+        .filterByStatus(FilterStatus.ONLY_ACTIVE)
+        .search();
     // @ts-ignore
-    
+
     dataTableAppointments = new DataTable('#tableListadoAppointments', {
         columns: [
             { data: 'id', title: 'Id' },
             { data: 'assignedDoctor.name', title: 'Doctor Asignado' },
-            { data: 'date', title: 'Fecha Turno', render: function ( data, type, row ) {
-                const dateEpoch = new Date(data);
-                const date = new Date(dateEpoch).toISOString().split("T")[0];
-                return date;
-            }},
+            {
+                data: 'date', title: 'Fecha Turno', render: function (data, type, row) {
+                    const dateEpoch = new Date(data);
+                    const date = new Date(dateEpoch).toISOString().split("T")[0];
+                    return date;
+                }
+            },
             { data: 'patient.name', title: 'Paciente Asignado' },
             { data: 'patient.dni', title: 'DNI Paciente' },
-            { data: 'remarks', title: 'Observaciones'},
+            { data: 'remarks', title: 'Observaciones' },
             { data: 'status', title: 'Estado' },
-            { data: 'active', render: function ( data, type, row ) {
-                return data ? "Activo" : "Inactivo";
-            }},
-            { data: '', render: function ( data, type, row ) {
-                return `<form action="./manage/index.html?Id=${row.id}" method="post"><button type="submit" class="btn btn-primary">Modificar Turno</button></form>`;
-            }},
-            { data: '', render: function ( data, type, row ) {
-                return `<button type="button" class="btn btn-primary" onclick="${row.active ? "disable" : "enable"}Appointment(${row.id});">${row.active ? "Desactivar" : "Activar"}</button>`;
-            }}
+            {
+                data: 'active', render: function (data, type, row) {
+                    return data ? "Activo" : "Inactivo";
+                }
+            },
+            {
+                data: '', render: function (data, type, row) {
+                    return `<button type="button" class="btn btn-success" onclick="presentAppointment(${row.id});">Presente</button>`;
+                }
+            },
+            {
+                data: '', render: function (data, type, row) {
+                    return `<button type="button" class="btn btn-primary" onclick="ausentAppointment(${row.id});">Ausente</button>`;
+                }
+            }
         ],
         data: appointments,
         paging: false,
         searching: false,
     });
-    btnPrevPage.classList.add("disabled");    
+    btnPrevPage.classList.add("disabled");
 };
 
 load().then(() => {
     isLoading = false;
-    if(!isLoading){
+    if (!isLoading) {
         loadingSpinner.classList.add("d-none")
     }
-    
-    const notification = new Toast({id: "notification"});
+
+    const notification = new Toast({ id: "notification" });
 
     const dataTableUpdate = async () => {
         dataTableAppointments.clear();
@@ -76,34 +85,30 @@ load().then(() => {
             .setQueryText(searchText)
             // @ts-ignore
             .paginate(page, parseInt(ddlEntriesPerPage.value))
-            // @ts-ignore
             .filterByAppointmentStatus(ddlStatusFilterAppointment.value)
-            .filterByStatus(FilterStatus[ddlStatusFilter.value])
+            .filterByDateBetween(new Date("2025-01-01").toISOString().split("T")[0], new Date("2025-02-01").toISOString().split("T")[0])
+            //.filterByDoctor()
+            .filterByStatus(FilterStatus.ONLY_ACTIVE)
             .search();
         dataTableAppointments.rows.add(appointments);
         dataTableAppointments.draw();
-        if(page == 1){
+        if (page == 1) {
             btnPrevPage.classList.add("disabled");
         } else {
             btnPrevPage.classList.remove("disabled")
         }
     }
-    
+
     ddlEntriesPerPage.onchange = () => {
         page = 1;
         loadingSpinner.classList.remove("d-none");
         isLoading = true;
-        dataTableUpdate().then(()=>{
+        dataTableUpdate().then(() => {
             isLoading = false;
-            if(!isLoading){
+            if (!isLoading) {
                 loadingSpinner.classList.add("d-none");
             }
         });
-    };
-
-    ddlStatusFilter.onchange = () => {
-        page = 1;
-        dataTableUpdate();
     };
 
     ddlStatusFilterAppointment.onchange = () => {
@@ -117,7 +122,7 @@ load().then(() => {
         searchText = txtBuscar.value;
         dataTableUpdate();
     });
-    
+
     btnPrevPage.addEventListener("click", async () => {
         if (page > 1) {
             page--;
@@ -129,7 +134,7 @@ load().then(() => {
             }
         }
     })
-    
+
     btnNextPage.addEventListener("click", async () => {
         page++;
         dataTableAppointments.clear();
@@ -139,29 +144,34 @@ load().then(() => {
             console.log(error);
         }
     })
-    
-    
-    const enableAppointment = async (id) => {
-        const enableResponse = await enable(id).then(() => {
-            dataTableUpdate();            
-            notification.setText("Turno Activado");
-            notification.setWarnColor(false);
-            notification.show();
-        })
+
+
+    const presentAppointment = async (id) => {
+        const appointment = await findById(id).then(async (response) => {
+            const obs = prompt("Observaciones Consulta:")
+            const presentResponse = await update(id, { ...response, status: "PRESENT", remarks: obs }).then(() => {
+                dataTableUpdate();
+                notification.setText("Marcado Presente");
+                notification.setWarnColor(false);
+                notification.show();
+            })
+        });
     }
-    
-    const disableAppointment = async (id) => {
-        const disableResponse = await disable(id).then(() => {
-            dataTableUpdate();
-            notification.setText("Turno Desactivado");
-            notification.setWarnColor(true);
-            notification.show();
-        })
+
+    const absentAppointment = async (id) => {
+        const appointment = await findById(id).then(async (response) => {
+            const ausentResponse = await update(id, { ...response, status: "ABSENT" }).then(() => {
+                dataTableUpdate();
+                notification.setText("Marcado Ausente");
+                notification.setWarnColor(true);
+                notification.show();
+            })
+        });
     }
-    
+
     // @ts-ignore
-    window.enableAppointment = enableAppointment;
+    window.presentAppointment = presentAppointment;
     // @ts-ignore
-    window.disableAppointment = disableAppointment;
+    window.absentAppointment = absentAppointment;
 });
 
