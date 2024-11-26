1ab47e67b1f1da6f665ef323847de7e870b139c6
Patricio_Bordon <patriciobordon123@gmail.com>
2024-11-25 22:29:16 -0300

    Revert "Reapply "refactorizacion Schedules""
    This reverts commit d6ee81e236128e4ba2ea53f4e16cf12747f3830d.

--- src/main/java/web/entity/Schedule.java
+++ src/main/java/web/entity/Schedule.java
@@ -27,8 +27,10 @@ import web.formatter.Card;
 public class Schedule {
 	
 	private int id;
-	private Day day;
-	private int time;
+	private Day beginDay;
+	private Day finishDay;
+	private Time startTime;
+	private Time endTime;
 	private boolean active;
 	
 	public Schedule() {}
@@ -43,25 +45,90 @@ public class Schedule {
 	}
 
 	@Enumerated(EnumType.STRING)
-	@Column(name = "day")
-	public Day getDay() {
-		return day;
+	@Column(name = "beginDay")
+	public Day getBeginDay() {
+		return beginDay;
+	}
+
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "finishDay")
+	public Day getFinishDay() {
+		return finishDay;
+	}
+	
+	
+	// Métodos problemáticos
+
+	@JsonIgnore
+	@Transient
+   // @Convert(converter = LocalTimeAttributeConverter.class)
+	public LocalTime getStartTimeLT() {
+		return startTime.toLocalTime();
 	}
 
 	@JsonIgnore
-	@Column(name = "time")
-	public int getTime() {
-	    return time;
+	@Transient
+    //@Convert(converter = LocalTimeAttributeConverter.class)
+	public LocalTime getEndTimeLT() {
+		return endTime.toLocalTime();
+	}
+
+	@JsonIgnore
+	//@JsonFormat(pattern = "HH:mm:ss")
+	@Column(name = "startTime")
+	public Time getStartTime() {
+	    return startTime;
+	}
+
+	@JsonIgnore
+	@Column(name = "endTime")
+	public Time getEndTime() {
+	    return endTime;
 	}
 
 	@Transient
-	@JsonProperty("time")
-	public String getTimeString() {
+	@JsonProperty("startTime")
+	public String getStartTimeString() {
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        return sdf.format(time);
+        return sdf.format(startTime);
+	}
+	
+	@Transient
+	@JsonProperty("endTime")
+	public String getEndTimeString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(endTime);
 	}
 
+	@Transient
+	public void setEndTimeString(String endTimeString) {
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        try {
+            java.util.Date date = sdf.parse(endTimeString);
+            Time time = new Time(date.getTime());
+            setEndTime(time);
+        } catch (ParseException e) {
+            System.out.println("Error al parsear la cadena: " + e.getMessage());
+        }
+	}
 
+	@Transient
+	public void setStartTimeString(String startTimeString) {
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        try {
+            java.util.Date date = sdf.parse(startTimeString);
+            Time time = new Time(date.getTime());
+            setStartTime(time);
+        } catch (ParseException e) {
+            System.out.println("Error al parsear la cadena: " + e.getMessage());
+        }
+	}
+	
+	
+	// Fin métodos problemáticos
+	
+	
 	@Column(name = "active")
 	public boolean isActive() {
 		return active;
@@ -73,12 +140,28 @@ public class Schedule {
 		this.id = id;
 	}
 
-	public void setDay(Day day) {
-		this.day = day;
+	public void setBeginDay(Day beginDay) {
+		this.beginDay = beginDay;
+	}
+
+	public void setFinishDay(Day finishDay) {
+		this.finishDay = finishDay;
 	}
 	
-	public void setTime(int time) {
-		this.time = time;
+	public void setStartTime(Time startTime) {
+		this.startTime = startTime;
+	}
+	
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = Time.valueOf(startTime);
+	}
+	
+	public void setEndTime(Time time) {
+		this.endTime = time;
+	}
+
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = Time.valueOf(endTime);
 	}
 	
 	public void setActive(boolean active) {
@@ -103,19 +186,25 @@ public class Schedule {
 			case FRIDAY: 
 				return "Viernes";
 			case SATURDAY: 
-				return "Sábado";
+				return "Sábados";
 			default: 
-				return "<?>";	
+				return "<?>";
 		}
 	}
 	
 	public String toString() {
-        String dayName = this.getWeekDaySpanishName(this.getDay());
-        int formattedTime = this.getTime();
-		return dayName + " " + formattedTime;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+		boolean sameDay = this.getBeginDay() == this.getFinishDay();
+		String startDayName = this.getWeekDaySpanishName(this.getBeginDay());
+		String endDayName = sameDay ? "" : " " + this.getWeekDaySpanishName(this.getFinishDay());
+        String startTime = this.getStartTimeLT().format(formatter);
+        String endTime = this.getEndTimeLT().format(formatter);
+        
+		return startDayName + " " + startTime + " -" + endDayName + " " + endTime;
 	}
 	
 	public int hashCode() {
 		return this.getId();
 	}
+	
 }
--- src/main/java/web/logic/validator/ScheduleValidator.java
+++ src/main/java/web/logic/validator/ScheduleValidator.java
@@ -19,6 +19,10 @@ public class ScheduleValidator {
 	
 	public Schedule check(Schedule schedule) throws ValidationException {
 		// Validar que el día de finalización sea el siguiente
+		if(schedule.getBeginDay().next() == schedule.getFinishDay())
+			throw new ValidationException("Invalid day range. ", "El día de finalización debe ser el mismo día o como mucho el día siguiente al día de inicio. Si necesita abarcar varios días, cree varios horarios para cada día. ");
+		if(schedule.getStartTimeLT().isAfter(schedule.getEndTimeLT()) && schedule.getBeginDay().next() == schedule.getFinishDay())
+			throw new ValidationException("Invalid time range. ", "La hora de salida debe ser posterior a la hora de inicio, salvo cuando el día de salida es posterior al día de inicio. ");
 		return schedule;
 	}
 	
